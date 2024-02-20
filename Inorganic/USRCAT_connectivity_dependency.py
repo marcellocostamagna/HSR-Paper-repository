cfb9a08e2e86605b8abebdb42981564ba0a940c4
Marcello Costamagna <marcello.costamagna@uib.no>
2024-02-20 02:41:33 +0100

    chnage name to continuity experiments

--- Inorganic/USRCAT_connectivity_dependency.py
+++ Inorganic/USRCAT_connectivity_dependency.py
@@ -15,19 +15,10 @@ molecules = load_molecules_from_sdf(f'{cwd}/connectivity_dependency.sdf', remove
 # Trans-Platin
 # PtCl2(NH3)2 Pt-ligands bonds = single bonds
 # PtCl2(NH3)2 Pt-NH3 bonds REMOVED (no charge adjustment)
-# PtCl2(NH3)2 Pt-Cl bonds REMOVED (charge adjustment: Pt(2+), Cl(-))
 
-### ROTATE MOLECULES ###
-rotated_molecules = []
-for molecule in molecules:
-    angle1 = np.random.randint(0, 360)
-    angle2 = np.random.randint(0, 360)
-    angle3 = np.random.randint(0, 360)
-    mol = rotate_molecule(molecule, angle1, angle2, angle3)
-    rotated_molecules.append(mol)
     
 n_molecules = len(molecules)
-usrcats = [GetUSRCAT(mol) for mol in rotated_molecules]
+usrcats = [GetUSRCAT(mol) for mol in molecules]
 print(f'USRCAT Similarity with rdkit:')
 for i in range(n_molecules):
     for j in range(i+1, n_molecules):

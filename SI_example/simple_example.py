7d7d14fd100cfbf2ed8f642cb82e8b33ece68007
Marcello Costamagna <marcello.costamagna@uib.no>
2024-03-07 11:46:42 +0100

    modify script to print all required steps

--- SI_example/simple_example.py
+++ SI_example/simple_example.py
@@ -13,12 +13,34 @@ cwd = os.getcwd()
 # List of molecules from SDF file
 molecules = load_molecules_from_sdf(f'{cwd}/simple_case.sdf', removeHs=False, sanitize=False)
 
-fingerprints = [generate_fingerprint_from_molecule(molecule, EXAMPLE_FEATURES, scaling='matrix') for molecule in molecules]
+# MOLECULES
+molecule_A = molecules[1]
+molecule_B = molecules[0]
 
-print(f'Fingerprints: \n{fingerprints[0]} \n{fingerprints[1]}')
 
-n_molecules = len(fingerprints)
-for i in range(n_molecules):
-    for j in range(i+1, n_molecules):
-        similarity = compute_similarity_score(fingerprints[i], fingerprints[j])
-        print(f"{i+1}-{j+1}: {similarity:.4f}")
+# PRE-PROCESSING
+molecule_A_matrix = molecule_to_ndarray(molecule_A, features=EXAMPLE_FEATURES)
+molecule_B_matrix = molecule_to_ndarray(molecule_B, features=EXAMPLE_FEATURES)
+
+print(f'Molecules initial matrix: \nmolecule_A:\n {molecule_A_matrix} \nmolecule_B\n {molecule_B_matrix}')
+
+
+# TODO: covariance, eigenvalues,eigenvectors and transformed data
+print(f'Molecule_A:\n')
+molecule_A_tranformed_data = compute_pca_using_covariance(molecule_A_matrix, print_steps=True)
+
+print(f'Molecule_B:\n')
+molecule_B_tranformed_data = compute_pca_using_covariance(molecule_B_matrix, print_steps=True)
+
+
+# FINGERPRINTS
+molecule_A_scaling = compute_scaling_matrix(molecule_A_tranformed_data)
+molecule_A_fingerprint = generate_fingerprint_from_transformed_data(molecule_A_tranformed_data, molecule_A_scaling)
+molecule_B_scaling = compute_scaling_matrix(molecule_B_tranformed_data)
+molecule_B_fingerprint = generate_fingerprint_from_transformed_data(molecule_B_tranformed_data, molecule_B_scaling)
+
+print(f'Fingerprints:\n \nmolecule_A:\n {molecule_A_fingerprint}\n \nmolecule_B\n {molecule_B_fingerprint}\n')
+
+# SIMILARITY
+similarity = compute_similarity_score(molecule_A_fingerprint, molecule_B_fingerprint)
+print(f'Similarity: {similarity:.4f}')
